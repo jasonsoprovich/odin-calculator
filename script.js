9bc28801542517ed529e13a69237c0a2f75245e3
Jason Soprovich <190052169+jasonsoprovich@users.noreply.github.com>
2025-02-08 17:49:35 -0700

    feat: add click functionality to numbers

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
-let a;
-let b;
+let firstNum;
+let secondNum;
 let operator; 
+
+const displayOutput = document.querySelector('#output');
+displayOutput.textContent = '0';
+
+const displayOperation = document.querySelector('#operation');
+displayOperation.textContent = 'a + b + c = 10';
+
+const numberButtons = document.querySelectorAll('.number');
+
+function getNumber(e){
+  displayOutput.textContent = e.target.textContent;
+  if (firstNum === undefined){
+    firstNum=e.target.textContent;
+  } else {
+    secondNum=e.target.textContent;
+  }
+  console.log(firstNum,'first');
+  console.log(secondNum,'second');
+  
+}
+
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', getNumber);
+})
+
+
  
 function add(a, b) {
   return a + b;
@@ -20,3 +47,11 @@ function divide(a, b) {
   return a / b;
 };
 
+
+// function clear() {
+
+// }
+
+function operate () {
+
+}
